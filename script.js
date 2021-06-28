daf0536b6cce2ff959018af1d9490817fbf43ddf
AlexDyabkin <alexendredyabkin@gmail.com>
2021-06-28 22:01:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-console.log(2);
-false
+const person = new Object({
+    name: 'Alex',
+    age: 25,
+    greet: function () {
+        console.log('Greet!')
+    } 
+})
+
+Object.prototype.sayHello = function(){
+    console.log('Hello')
+}
+
+const lena = Object.create(person)
+lena.name = 'Elena'
+
+
+
+const str = new String('I am string')
+
+
